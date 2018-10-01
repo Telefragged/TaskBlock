c84d7d9e04e09463c03d98a4c36892dc80183eaf
Telefragged <Vetle.braend@gmail.com>
2018-10-01 23:27:58 +0200

    Fix rest of indentation

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -12,22 +12,22 @@
 
 class ThreadPool
 {
-	std::vector<std::thread> workerThreads_;
+    std::vector<std::thread> workerThreads_;
 
-	moodycamel::BlockingConcurrentQueue<std::function<void()>> fnQueue_;
+    moodycamel::BlockingConcurrentQueue<std::function<void()>> fnQueue_;
 
-	bool working_ = true;
+    bool working_ = true;
 
     std::atomic_size_t num_queued_or_running_ = 0;
 
-	void worker_loop()
-	{
-		while (true)
-		{
-			std::function<void()> fn;
+    void worker_loop()
+    {
+        while (true)
+        {
+            std::function<void()> fn;
 
-			if (!working_ && fnQueue_.size_approx() == 0)
-				return;
+            if (!working_ && fnQueue_.size_approx() == 0)
+                return;
 
             bool got_value = false;
 
@@ -41,24 +41,24 @@ class ThreadPool
             if (!got_value)
                 return;
 
-			fn();
+            fn();
             --num_queued_or_running_;
-		}
-	}
+        }
+    }
 
 public:
-	template <class Fn, class... Args>
-	std::future<std::invoke_result_t<Fn, Args...>> post(Fn &&fn, Args&&... args)
-	{
-		using ReturnType = std::invoke_result_t<Fn, Args...>;
+    template <class Fn, class... Args>
+    std::future<std::invoke_result_t<Fn, Args...>> post(Fn &&fn, Args&&... args)
+    {
+        using ReturnType = std::invoke_result_t<Fn, Args...>;
 
         if (working_ == false)
             throw std::exception();
 
-		auto promise = std::make_shared<std::promise<ReturnType>>();
+        auto promise = std::make_shared<std::promise<ReturnType>>();
 
-		if constexpr (std::is_void_v<ReturnType>)
-		{
+        if constexpr (std::is_void_v<ReturnType>)
+        {
             fnQueue_.enqueue(
             [
                 promise,
@@ -69,9 +69,9 @@ public:
                 std::apply(func, std::move(args));
                 promise->set_value();
             });
-		}
-		else
-		{
+        }
+        else
+        {
 
             fnQueue_.enqueue(
                 [
@@ -82,12 +82,12 @@ public:
             {
                 promise->set_value(std::apply(func, std::move(args)));
             });
-		}
+        }
 
         ++num_queued_or_running_;
 
-		return promise->get_future();
-	}
+        return promise->get_future();
+    }
 
     template <class Fn, class... Args>
     void post_no_future(Fn &&fn, Args&&... args)
@@ -104,17 +104,17 @@ public:
         ++num_queued_or_running_;
     }
 
-	ThreadPool(size_t numWorkers)
-	{
-		for (size_t n = 0; n < numWorkers; n++)
-			workerThreads_.emplace_back(&ThreadPool::worker_loop, this);
-	}
+    ThreadPool(size_t numWorkers)
+    {
+        for (size_t n = 0; n < numWorkers; n++)
+            workerThreads_.emplace_back(&ThreadPool::worker_loop, this);
+    }
 
-	~ThreadPool()
-	{
-		working_ = false;
+    ~ThreadPool()
+    {
+        working_ = false;
 
         for (auto &worker : workerThreads_)
             worker.join();
-	}
+    }
 };
